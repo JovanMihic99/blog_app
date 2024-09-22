176c3c4f53398299adf075a1f3eca167a53338af
JovanMihic99 <jovanmihic99@gmail.com>
2024-09-22 21:18:07 +0200

    chore: css tweaks in post page

--- views/home.php
+++ views/home.php
@@ -9,16 +9,16 @@
             <?php foreach ($posts as $post): ?>
                 <div class="blog-post">
                     <li>
-                        <a href="/blog_app/user.php?user_id=<?php echo htmlspecialchars($post['user_id']) ?>">
-                            <?php echo htmlspecialchars($post['user_name']) ?>
+
+                        <a class="username" href="/blog_app/user.php?user_id=<?php echo htmlspecialchars($post['user_id']) ?>">
+                            <?php echo   htmlspecialchars($post['user_name']) ?>
                         </a>
                         <br>
-                        <small>Posted on <?php echo htmlspecialchars($post['created_timestamp']); ?></small>
+
                         <a href="/blog_app/post.php?blog_id=<?php echo htmlspecialchars($post['blog_id']) ?>">
                             <h2><?php echo htmlspecialchars($post['title']); ?></h2>
 
-
-
+                            <small>Posted on <?php echo htmlspecialchars($post['created_timestamp']); ?></small>
                             <p class="post-content"><?php echo htmlspecialchars(substr($post['content'], 0, 250) . '...'); ?></p>
                         </a>
                     </li>
--- views/post.php
+++ views/post.php
@@ -13,36 +13,37 @@
     <p> <a href="/blog_app/login.php" style="text-decoration: underline">Login</a> to leave a comment</p>
 <?php endif; ?>
 <?php if (!empty($comments)): ?>
-    <ul>
-        <?php foreach ($comments as $comment): ?>
+    <div class="comments-wrapper">
+        <ul>
+            <?php foreach ($comments as $comment): ?>
 
-            <div class="comment_container">
+                <div class="comment-container">
 
-                <li class="comment" id="comment-<?php echo $comment['comment_id'] ?>">
+                    <li class="comment" id="comment-<?php echo $comment['comment_id'] ?>">
 
-                    <a class="comment_username" href="/blog_app/user.php?user_id=<?php echo htmlspecialchars($comment['user_id']) ?>">
-                        <?php echo htmlspecialchars($comment['user_name']); ?>
-                    </a>
-                    <small>Commented on <?php echo htmlspecialchars($comment['created_at']); ?></small>
-                    <br>
-                    <p class="comment-content"><?php echo htmlspecialchars($comment['content']) ?></p>
-
-                    <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] === $comment['user_id']): ?>
-                        <a href="/blog_app/delete_comment.php?comment_id=<?php echo $comment['comment_id']; ?>"
-                            class="delete-comment"
-                            onclick="return confirm('Are you sure you want to delete this comment?');">
-                            DELETE
+                        <a class="comment_username" href="/blog_app/user.php?user_id=<?php echo htmlspecialchars($comment['user_id']) ?>">
+                            <?php echo htmlspecialchars($comment['user_name']); ?>
                         </a>
-                        <?php echo $_SESSION['user_id']; ?>
-                    <?php endif; ?>
+                        <small>Commented on <?php echo htmlspecialchars($comment['created_at']); ?></small>
+                        <br>
+                        <p class="comment-content"><?php echo htmlspecialchars($comment['content']) ?></p>
+
+                        <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] === $comment['user_id']): ?>
+                            <a href="/blog_app/delete_comment.php?comment_id=<?php echo $comment['comment_id']; ?>"
+                                class="delete-comment"
+                                onclick="return confirm('Are you sure you want to delete this comment?');">
+                                DELETE
+                            </a>
+                            <?php echo $_SESSION['user_id']; ?>
+                        <?php endif; ?>
 
 
-                </li>
-            </div>
+                    </li>
+                </div>
 
-        <?php endforeach; ?>
-    </ul>
-
+            <?php endforeach; ?>
+        </ul>
+    </div>
 <?php else: ?>
     <hr>
     <p>No comments available.</p>
